cfd3222e86404a57e38196199635b6714c4ed563
Vasileios Karavasilis <cyane.spark@gmail.com>
2026-01-30 19:52:10 +0200

    updated weapon to bullet

--- custom resources/weapon.gd
+++ custom resources/weapon.gd
@@ -1,4 +1,4 @@
-class_name Weapon
+class_name Bullet
 extends Resource
 
 enum ShotType{
@@ -6,8 +6,15 @@ enum ShotType{
 	RAPID,
 }
 
+@export_group("Bullet Properties")
 @export var shots_per_seconds: float = 1.0
 @export var bullets_per_shot: int = 1
 @export var shot_type: ShotType = ShotType.RAPID
+@export var min_bullet_speed: float = 100.0:
+	set(value):
+		min_bullet_speed = minf(value, max_bullet_speed)
+@export var max_bullet_speed: float = 100.0:
+	set(value):
+		max_bullet_speed = maxf(value, min_bullet_speed)
 
 # *************************************
