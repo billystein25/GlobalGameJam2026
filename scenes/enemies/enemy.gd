bfeb4a908f93254150245515baa4dc063b49d06b
Vasileios Karavasilis <cyane.spark@gmail.com>
2026-01-31 22:06:16 +0200

    spawner

--- scenes/enemies/enemy.gd
+++ scenes/enemies/enemy.gd
@@ -30,11 +30,15 @@ var knockback = Vector2.ZERO
 
 func _ready() -> void:
 	time_to_shoot.timeout.connect(_on_request_shoot)
-	time_to_shoot.wait_time = enemy_resource.bullet.seconds_per_shot
-	animated_sprite_2d.sprite_frames = enemy_resource.sprite_anime
-	animated_sprite_2d.offset = enemy_resource.offset
-	collision_shape_2d.shape = enemy_resource.collision_box
+	set_values(enemy_resource)
+
+func set_values(resource: Enemy_resource) -> void:
+	time_to_shoot.wait_time = resource.bullet.seconds_per_shot
+	animated_sprite_2d.sprite_frames = resource.sprite_anime
+	animated_sprite_2d.offset = resource.offset
+	collision_shape_2d.shape = resource.collision_box
 	
+
 
 func _physics_process(delta: float) -> void:
 	
